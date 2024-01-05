240a6c4ff31a1d116bc51172943a4d1cbe45c61d
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2024-01-05 05:19:38 +0100

    finished the landing page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import WrapperComponent from './components/WrapperComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <WrapperComponent />
+    </div>
+  );
+}
+
+export default App;
